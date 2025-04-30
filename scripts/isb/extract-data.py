8a4501c179588dfb62124b1c61c4eeecfd805734
Yi Liu <yi.liu6240@gmail.com>
2025-04-30 09:40:13 +0000

    Update extract-data logic

--- scripts/isb/extract-data.py
+++ scripts/isb/extract-data.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import torch
 from environs import env
 from tqdm import tqdm
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, QuantoConfig
 
 from local_funcs import chat_funcs, prompt_funcs
 from yiutils.project_utils import find_project_root
@@ -24,22 +24,41 @@ def main():
         default=proj_root / "output",
         help="Directory to save the output JSON file. Defaults to 'output' in the project root.",
     )
+    parser.add_argument(
+        "--path_data",
+        type=str,
+        default=None,
+        help="Path to pubmed.json",
+    )
+    parser.add_argument(
+        "--pilot",
+        action="store_true",
+        help="Enable pilot mode. Defaults to False.",
+    )
     args = parser.parse_args()
+    print(f"args: {args}")
 
     # params
+    # {{{
     array_task_id = env.int("SLURM_ARRAY_TASK_ID")
+    access_token = env("HUGGINGFACE_TOKEN")
     num_docs = 100
-    startpoint = array_task_id * num_docs
-    endpoint = startpoint + num_docs + 1
-    data_path = proj_root / "data"
+    if args.pilot:
+        print("Running in pilot mode with 100 documents.")
+        startpoint = 0
+        endpoint = 101
+    else:
+        startpoint = array_task_id * num_docs
+        endpoint = startpoint + num_docs + 1
+
     output_dir = Path(args.output_dir)
     output_dir.mkdir(parents=True, exist_ok=True)
     out_file = output_dir / f"mr_extract_llama3_sample_array_{array_task_id}.json"
-    access_token = env("HUGGINGFACE_TOKEN")
+    # }}}
 
     # Get abstracts
-    path_to_pubmed = data_path / "raw" / "mr-pubmed-abstracts" / "data" / "pubmed.json"
-    assert path_to_pubmed.exists(), print("pubmed.json not found")
+    path_to_pubmed = Path(args.path_data)
+    assert args.path_data is not None and path_to_pubmed.exists(), print("pubmed.json not found")
     with path_to_pubmed.open("r") as f:
         pubmed = json.load(f)
     print("Loaded abstracts")
@@ -49,12 +68,22 @@ def main():
     device = "cuda"
     dtype = torch.bfloat16
     tokenizer = AutoTokenizer.from_pretrained(model_id, token=access_token)
-    model = AutoModelForCausalLM.from_pretrained(
-        model_id,
-        torch_dtype=dtype,
-        device_map=device,
-        token=access_token,
-    )
+    if args.pilot:
+        quantization_config = QuantoConfig(weights="int4")
+        model = AutoModelForCausalLM.from_pretrained(
+            model_id,
+            torch_dtype=dtype,
+            device_map=device,
+            token=access_token,
+            quantization_config=quantization_config,
+        )
+    else:
+        model = AutoModelForCausalLM.from_pretrained(
+            model_id,
+            torch_dtype=dtype,
+            device_map=device,
+            token=access_token,
+        )
     print("Loaded model")
 
     fulldata = []
