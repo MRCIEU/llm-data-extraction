6e84fac688476f2f549d00a682f0be199451add8
Yi Liu <yi.liu6240@gmail.com>
2025-06-20 12:02:56 +0100

    Update processing

--- scripts/python/process-llm-aggregated-results.py
+++ scripts/python/process-llm-aggregated-results.py
@@ -9,20 +9,21 @@ data / intermediate / <MODEL-NAME> / processed_results.json.
 """
 
 import pandas as pd
+from loguru import logger
 
 from local_funcs import parsers
 from yiutils.project_utils import find_project_root
 
 
 def process_deepseek_r1_distilled(model_config):
-    # ---- raw results ----
+    # ---- read raw results ----
     path_to_raw_results = model_config["data_dir"] / "raw_results.json"
     assert path_to_raw_results.exists(), (
         f"Raw results file does not exist: {path_to_raw_results}"
     )
     raw_results_df = pd.read_json(path_to_raw_results, orient="records")
 
-    # ---- processed results ----
+    # ---- process results ----
     results_df = raw_results_df.assign(
         metadata_thinking=lambda df: df["completion_metadata"].apply(
             parsers.extract_thinking
@@ -54,14 +55,14 @@ def process_deepseek_r1_distilled(model_config):
 
 
 def process_llama3_2(model_config):
-    # ---- raw results ----
+    # ---- read raw results ----
     path_to_raw_results = model_config["data_dir"] / "raw_results.json"
     assert path_to_raw_results.exists(), (
         f"Raw results file does not exist: {path_to_raw_results}"
     )
     raw_results_df = pd.read_json(path_to_raw_results, orient="records")
 
-    # ---- processed results ----
+    # ---- process results ----
     results_df = raw_results_df.assign(
         metadata=lambda df: df["completion_metadata"].apply(parsers.parse_json),
         results=lambda df: df["completion_results"].apply(parsers.parse_json),
@@ -81,14 +82,14 @@ def process_llama3_2(model_config):
 
 
 def process_llama3(model_config):
-    # ---- raw results ----
+    # ---- read raw results ----
     path_to_raw_results = model_config["data_dir"] / "raw_results.json"
     assert path_to_raw_results.exists(), (
         f"Raw results file does not exist: {path_to_raw_results}"
     )
     raw_results_df = pd.read_json(path_to_raw_results, orient="records")
 
-    # ---- processed results ----
+    # ---- process results ----
     results_df = raw_results_df[
         [
             "pmid",
@@ -127,8 +128,13 @@ def main():
             f"Input path for {k} does not exist: {v['data_dir']}"
         )
 
+    logger.info("deepseek-r1-distilled")
     process_deepseek_r1_distilled(model_configs["deepseek-r1-distilled"])
+
+    logger.info("llama3")
     process_llama3(model_configs["llama3"])
+
+    logger.info("llama3_2")
     process_llama3_2(model_configs["llama3-2"])
 
 
