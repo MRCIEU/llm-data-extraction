6905d2f5a08366b944e2a19b6e3b0bfc0188781e
Yi Liu <yi.liu6240@gmail.com>
2025-07-21 16:34:39 +0100

    update notebook

--- notebooks/sanity/extract-data-openai.marimo.py
+++ notebooks/sanity/extract-data-openai.marimo.py
@@ -7,6 +7,7 @@ app = marimo.App(width="full")
 @app.cell
 def _():
     import marimo as mo
+
     return (mo,)
 
 
@@ -14,10 +15,12 @@ def _():
 def module_init():
     # Import the extract_data module
     from yiutils.project_utils import find_project_root
+
     project_root = find_project_root("justfile")
     path_to_script = project_root / "scripts" / "python" / "extract-data-openai.py"
 
     import importlib.util
+
     spec = importlib.util.spec_from_file_location("extract_data", str(path_to_script))
     extract_data = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(extract_data)
@@ -41,7 +44,7 @@ def init(extract_data):
     load_schema_data = extract_data.load_schema_data
     setup_openai_client = extract_data.setup_openai_client
 
-    return get_config, load_schema_data, setup_openai_client
+    return get_config, json, load_schema_data, setup_openai_client
 
 
 @app.cell
@@ -81,7 +84,7 @@ def client(config, setup_openai_client):
     client = setup_openai_client(api_key=config["openai_api_key"])
     print("OpenAI client initialized")
 
-    return (client,)
+    return
 
 
 @app.cell
@@ -107,20 +110,20 @@ def _(mo):
 
 
 @app.cell
-def process_abstract(client, config, extract_data, pubmed_data, schema_data):
+def process_abstract(pubmed_data):
     article_data = pubmed_data[0]
     print(article_data.keys())
 
-    process_abstract = extract_data.process_abstract
-    output = process_abstract(
-        article_data=article_data,
-        schema_data=schema_data,
-        client=client,
-        model_config=config["model_config"],
-    )
+    # process_abstract = extract_data.process_abstract
+    # output = process_abstract(
+    #     article_data=article_data,
+    #     schema_data=schema_data,
+    #     client=client,
+    #     model_config=config["model_config"],
+    # )
 
-    print(output, article_data)
-    return
+    # print(output, article_data)
+    return (article_data,)
 
 
 @app.cell(hide_code=True)
@@ -136,12 +139,18 @@ def _(mo):
 
 
 @app.cell
-def _(config, article_data, schema_data):
-    from local_funcs import prompt_funcs
+def _(mo):
+    mo.md(r"""## prompts""")
+    return
+
+
+@app.cell
+def prompt_metadata(article_data, json, project_root, schema_data):
     from pprint import pprint
 
-    model_config = config["model_config"]
-    chat_func = model_config["chat_func"]
+    from local_funcs import prompt_funcs
+
+    # model_config = config["model_config"]
 
     input_prompt_metadata = prompt_funcs.make_message_metadata_new(
         abstract=article_data["ab"],
@@ -149,6 +158,23 @@ def _(config, article_data, schema_data):
         json_schema=schema_data["metadata"]["schema"],
     )
     pprint(input_prompt_metadata)
+
+    with (project_root / "output" / "input_prompt_metadata.json").open("w") as _:
+        json.dump(input_prompt_metadata, _, indent=2)
+    return pprint, prompt_funcs
+
+
+@app.cell
+def _(article_data, json, pprint, project_root, prompt_funcs, schema_data):
+    input_prompt_results = prompt_funcs.make_message_metadata_new(
+        abstract=article_data["ab"],
+        json_example=schema_data["results"]["example"],
+        json_schema=schema_data["results"]["schema"],
+    )
+    pprint(input_prompt_results)
+
+    with (project_root / "output" / "input_prompt_results.json").open("w") as _:
+        json.dump(input_prompt_results, _, indent=2)
     return
 
 
